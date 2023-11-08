59cd0d337ad0e8fe136d16db142559be3cd1e059
happyjason <>
2023-11-08 19:35:37 +0800

    update token bound

--- src/components/Awns/Drawer/SendNFTDrawer.tsx
+++ src/components/Awns/Drawer/SendNFTDrawer.tsx
@@ -101,8 +101,8 @@ const Page = ({
       new TokenboundClient({
         signer: signer.data,
         chainId,
-        implementationAddress: '0x2d25602551487c3f3354dd80d76d54383a243358',
-        registryAddress: '0x02101dfB77FDE026414827Fdc604ddAF224F0921',
+        // implementationAddress: '0x2d25602551487c3f3354dd80d76d54383a243358',
+        // registryAddress: '0x02101dfB77FDE026414827Fdc604ddAF224F0921',
       }),
     [chainId, signer.data],
   )
