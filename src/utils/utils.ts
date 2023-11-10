e89b22a7f4d1beba25e599970f9a8d47ed65eb0f
bill <>
2023-11-10 12:40:27 +0800

    update yearsToSeconds for hours

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -42,7 +42,8 @@ export const secondsToHours = (seconds: number) => Math.floor(seconds / (60 * 60
 
 export const daysToSeconds = (days: number) => days * 60 * 60 * 24
 
-export const yearsToSeconds = (years: number) => years * 60 * 60 * 24 * 365
+export const yearsToSeconds = (years: number) => years * 60 * 60
+// export const yearsToSeconds = (years: number) => years * 60 * 60 * 24 * 365
 
 export const secondsToYears = (seconds: number) => seconds / (60 * 60 * 24 * 365)
 
